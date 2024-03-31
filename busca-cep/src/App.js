20493cdbff4ab7b4d494f3d88789dc6b43cd4ce8
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-30 21:46:26 -0400

    arrumei um bug na minha useState troquei {} por []

--- busca-cep/src/App.js
+++ busca-cep/src/App.js
@@ -1,38 +1,42 @@
-import { useState } from 'react';
-import { FiSearch} from 'react-icons/fi';
-import'./styles.css';
+import React, { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
 
 function App() {
-
-  const {input, setInput} = useState()
+  const [input, setInput] = useState("");
 
   function handleSearch() {
-      alert("Valor do Input " + input)
+    if (input === "") {
+      alert("Preencha algum cep!");
+      return;
+    }
+    // Lógica de busca aqui
   }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
 
-        <div className="containerInput">
-          <input 
+      <div className="containerInput">
+        <input 
           type="text"
           placeholder="Digite seu cep:"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          />
+        />
 
-          <button className="buttonSearch" onClick={handleSearch}>
-            <FiSearch size={25} color="#fff"/>
-          </button>
-        </div>
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
 
-        <main className="main">
-            <h2>CEP: </h2>
-            <span>Rua Teste Algum</span>
-            <span>Complemento:</span>
-            <span>Bairro</span>
-            <span>Sinop-MT</span>
-        </main>
+      <main className="main">
+          <h2>CEP: </h2>
+          <span>Rua Teste Algum</span>
+          <span>Complemento:</span>
+          <span>Bairro</span>
+          <span>Sinop-MT</span>
+      </main>
     </div>
   );
 }

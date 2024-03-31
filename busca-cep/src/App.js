1ac49dee8563ede821f795a7345bfe7601016ec1
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-30 21:24:12 -0400

    comiit pre final

--- busca-cep/src/App.js
+++ busca-cep/src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { FiSearch} from 'react-icons/fi';
 import'./styles.css';
 
 function App() {
+
+  const {input, setInput} = useState()
+
+  function handleSearch() {
+      alert("Valor do Input " + input)
+  }
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -10,9 +17,11 @@ function App() {
           <input 
           type="text"
           placeholder="Digite seu cep:"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           />
 
-          <button className="buttonSearch">
+          <button className="buttonSearch" onClick={handleSearch}>
             <FiSearch size={25} color="#fff"/>
           </button>
         </div>
